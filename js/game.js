eb1abfc9b4b26eb3d68113d3f30ec4e54dad4211
Yash1234567891011 <73171838+Yash1234567891011@users.noreply.github.com>
2022-09-20 16:39:54 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent("game-play", {
+    schema: {
+        elementId: { type: "string", default: "#coin1" }
+    },
+    update: function () {
+        this.iscollided(this.data.elementId)
+    },
+    iscollided: function (elementId) {
+        const element = document.querySelector(elementId);
+        element.addEventListener("collide", (e) => {
+            if (elementId.includes("#coin")) {
+                element.setAttribute("visible", false);
+                console.log("ring collision");
+            }
+            if (elementId.includes("#fish")) {
+                console.log("fish collision");
+            }
+        })
+    }
+})
